7c1524687b8eb881edb96d5d150b56b0e1de402f
abhi121shah <abhijeetshah008@gmail.com>
2025-09-01 16:12:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 alert("This page is under devlopment")
-function toggleNav() {
-  let nav = document.getElementById("sidenav");
-  if (nav.style.width === "250px") {
-    nav.style.width = "0";
-  } else {
-    nav.style.width = "250px";
-  }
-}
